51653ffd55d125b7e43280bf41a444b0f14e1f5f
Mohan raj <731119104028mohanraj.p@gmail.com>
2023-08-21 15:19:36 +0530

    next program

---
+++ if/smallest_of_3_numbers.py
@@ -0,0 +1,12 @@
+i = int(input("Enter the i: "))
+j = int(input("Enter the j: "))
+k = int(input("Enter the k: "))
+
+if(i<j and i<k):
+    print(i,"is smallest number")
+
+elif(j<i and j<k):
+    print(j,"is smallest number")
+
+else:
+    print(k,"is smallest number")        
