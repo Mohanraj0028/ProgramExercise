fe1d7fd11ce411c8c4d47571541617f8ea09bb80
Mohan raj <731119104028mohanraj.p@gmail.com>
2023-08-21 18:23:56 +0530

    10_if_program

---
+++ if/addsubmuldiv.py
@@ -0,0 +1,20 @@
+n = int(input("Enther the n: "))
+
+m = int(input("Enter the m: " ))
+
+str = input("Enter the operation: ")
+
+
+if str == "ADD":
+    print(n+m)
+
+elif str == "SUB" :
+    print(n-m)
+
+elif str == "MUL" :
+    print(n-m)
+
+elif str == "DIV" :
+    print(n-m)        
+
+   
