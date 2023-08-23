ed1f7e332c2cf088a4c6933d437e901b427b3305
Mohan raj <731119104028mohanraj.p@gmail.com>
2023-08-23 14:12:50 +0530

    12

--- doubleloop/66.py
+++ doubleloop/66.py
@@ -1,4 +1,14 @@
 n = int(input("Enter: "))
 
-for i in range(1,n+1):
-    print (i)
+f = "black"
+for i in range(1, n+1):
+
+    for j in range(1, n+1):
+        print(f, end="")
+        if j < n:
+            print("-", end="")
+        if f == "white":
+            f = "black"
+        else:
+            f = "white"
+    print() 
